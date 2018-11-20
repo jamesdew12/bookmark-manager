afdb8e4a8b2a9d8e8d00adb29b73b2b674515497
James de Winton <james.dewinton@gmail.com>
2018-11-20 15:01:46 +0000

    ENV updated to set to test

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -4,6 +4,10 @@ class Bookmark
 
 
   def self.all
+    if ENV['ENVIROMENT'] == 'test'
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+    else
+
     connection = PG.connect(dbname: 'bookmark_manager')
     result = connection.exec("SELECT * FROM bookmarks;")
     result.map { |bookmark| bookmark['url'] }
