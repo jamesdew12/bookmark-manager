345e5469ed3ed7bea3af04d477fbf7899928fafc
James de Winton <james.dewinton@gmail.com>
2018-11-23 10:18:47 +0000

    final day of week beginning of 12

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -2,6 +2,13 @@ require 'pg'
 
 class Bookmark
 
+    attr_reader :id, :title, :url
+
+    def initialize(id:, title:, url:)
+      @id  = id
+      @title = title
+      @url = url
+    end
 
   def self.all
     if ENV['ENVIROMENT'] == 'test'
@@ -11,18 +18,17 @@ class Bookmark
     connection = PG.connect(dbname: 'bookmark_manager')
     end
     result = connection.exec("SELECT * FROM bookmarks;")
-    result.map { |bookmark| bookmark['title'] }
+    result.map { |bookmark| bookmark }
   end
 
   def self.create(url:, title:)
-    if ENV['ENVIROMENT'] == 'test'
-    connection = PG.connect(dbname: 'bookmark_manager_test')
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'bookmark_manager_test')
     else
-
-    connection = PG.connect(dbname: 'bookmark_manager')
+      connection = PG.connect(dbname: 'bookmark_manager')
     end
-    connection.exec("INSERT INTO bookmarks (url, title) VALUES('#{url}', '#{title}');")
-
+    result = connection.exec("INSERT INTO bookmarks (url, title) VALUES('#{url}', '#{title}') RETURNING id, title, url;")
+    Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
   end
 
 end
--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -20,9 +20,14 @@ require './lib/bookmark'
     end
   end
   describe '.create' do
-    it 'creates a new bookmark and adds it to the database' do
-      Bookmark.create(url: 'test', title: 'test title')
-      expect(Bookmark.all).to include ('test title')
+    it 'creates a new bookmark' do
+      bookmark = Bookmark.create(url: 'http://www.testbookmark.com', title: 'Test Bookmark')
+      persisted_data = PG.connect(dbname: 'bookmark_manager_test').query("SELECT * FROM bookmarks WHERE id = #{bookmark.id};")
+
+      expect(bookmark).to be_a Bookmark
+      expect(bookmark.id).to eq persisted_data.first['id']
+      expect(bookmark.title).to eq 'Test Bookmark'
+      expect(bookmark.url).to eq 'http://www.testbookmark.com'
     end
   end
 
@@ -30,4 +35,15 @@ require './lib/bookmark'
 
 
 
+end
+describe '.create' do
+  it 'creates a new bookmark' do
+    bookmark = Bookmark.create(url: 'http://www.testbookmark.com', title: 'Test Bookmark')
+    persisted_data = PG.connect(dbname: 'bookmark_manager_test').query("SELECT * FROM bookmarks WHERE id = #{bookmark.id};")
+
+    expect(bookmark).to be_a Bookmark
+    expect(bookmark.id).to eq persisted_data.first['id']
+    expect(bookmark.title).to eq 'Test Bookmark'
+    expect(bookmark.url).to eq 'http://www.testbookmark.com'
+  end
 end
